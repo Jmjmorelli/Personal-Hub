72e04c055d0eba8300b74c2a5fc710fcd386958b
Jmjmorelli@Linux <jvgmorelli@gmail.com>
2026-06-28 20:30:04 -0400

    Updated Logs

--- morelli/app/logs/page.tsx
+++ morelli/app/logs/page.tsx
@@ -38,6 +38,14 @@ export default function Logs() {
             </h1>
             <p>Shared files directory live, served through the server. Using as my own cloud storage! As of logging, currently at 500 GB of space.</p>
             <p>Subdomain routed to the directory</p>
+
+            <h1 style={{ fontSize: "1.25rem", fontWeight: "bold", marginTop: "1rem" }}>
+              June 28th 2026
+            </h1>
+            <p>Linuxshare and morelliServer folders created, on Jupiter and Mercury. Samba succesfully configured on the server!</p>
+            <p>Able to view through all devices on same network, can directly upload files to my cloud! Maybe I should call it morelliCloud</p>
+            {/* <p>Configured watch and drop script on the server to listen to new file uploads and automatically migrate them to the shared directory.</p> */}
+            
           </div>
         </div>
       </div>
